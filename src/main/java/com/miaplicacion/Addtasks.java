91470027387e9e45c99c33eaa35e07c93484b13e
Krisel1 <kriselur@gmail.com>
2024-06-04 15:05:01 +0200

    Modificacion archivo

---
+++ src/main/java/com/miaplicacion/Addtasks.java
@@ -0,0 +1,16 @@
+package com.miaplicacion;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Addtasks {
+    static ArrayList<String> tasks = new ArrayList<>();
+    static void addTask() {
+            System.out.print("Enter the description of the new task: ");
+            Scanner sc = new Scanner(System.in);
+            String description = sc.nextLine();
+            tasks.add(description);
+            System.out.println("Task added successfully.");
+            sc.close();
+    }
+}
--- src/main/java/com/miaplicacion/App.java
+++ src/main/java/com/miaplicacion/App.java
@@ -1,17 +1,12 @@
 package com.miaplicacion;
 
-/**
- * Hello world!
- */
 public final class App {
+    private static final String Addtask = null;
     private App() {
     }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Addtasks.addTask(); 
     }
 }
